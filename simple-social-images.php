160dc52151aeea20d167e57d81b9a423d39ee9e0
Mark Wilkinson <mark@highrise.digital>
2022-08-19 09:16:45 +0100

    Set default text align on plugin activation

--- simple-social-images.php
+++ simple-social-images.php
@@ -96,6 +96,14 @@ function hd_ssi_on_activation() {
 
 	}
 
+	// if we have no font style.
+	if ( empty( hd_ssi_get_text_alignment() ) ) {
+
+		// set the default bg color.
+		update_option( 'hd_ssi_text_align', 'default' );
+
+	}
+
 	// flush the rewrite rules.
 	flush_rewrite_rules();
 
